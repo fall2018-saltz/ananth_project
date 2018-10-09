cf4651a9b25d5cf0ad9d2fd97e4ee48d75870b81
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-09 14:06:16 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -1,4 +1,4 @@
 
 clean_data <- raw_data
 
-alm <- read.csv(url("https://www2.census.gov/programs-surveys/popest/datasets/2010-2017/state/asrh/scprc-est2017-18+pop-res.csv"), stringsAsFactors=FALSE)
+clean_data <- read.csv(url("https://www2.census.gov/programs-surveys/popest/datasets/2010-2017/state/asrh/scprc-est2017-18+pop-res.csv"), stringsAsFactors=FALSE)
